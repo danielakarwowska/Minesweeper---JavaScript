81b6e3b440ced47805806352798f893138dc7d97
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-06-12 20:01:48 +0200

    randomly setting bombs

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
-const button = document.querySelector("button");
-const showBoard = document.querySelector("#board");
-const arr = [];
+const numOfRows = 12;
+const numOfColumn = 12;
+const numBombs = 45;
+const bombs = "💣";
+const alive = true;
+let colors = [
+  "blue",
+  "green",
+  "red",
+  "purple",
+  "maroon",
+  "turquoise",
+  "black",
+  "grey",
+];
 
-const show = () => {
-  let text = "";
-  for (y = 0; y < 8; y++) {
-    for (x = 0; x < 8; x++) {
-      text += arr[y][y] + "";
-    }
-    text += ""; //after each line, to the next line
-    showBoard.innerHTML = text;
-  }
+window.addEventListener("load", function () {
+  document.querySelector("lost").style.display = "none";
+  startGame();
+}); //calling startGame () each time the page is loaded
+
+const startGame = () => {
+  bombs = placeBombs();
+  document.querySelector("field").append(createTable());
+}; //This function is used to create a table and initialize bombs
+
+const idCell = (x, y) => {
+  return "cell-" + x + "-" + y;
 };
 
-const start = () => {
-  arr = []; //clearing the board
-  for (let y = 0; x < 8; x++) {
-    //lines 0 through 8
-    arr[y] = [];
-    for (let x = 0; x < 8; x++) {
-      //columns 0 to 8
-      arr[y][x] = 0; //reset each field of the board
+const createTable = () => {
+  let table, row, td, x, y;
+  table = document.createElement("table");
+
+  for (x = 0; x < numOfRows; x++) {
+    row = document.createElement("tr");
+    for (y = 0; y < numOfColumn; y++) {
+      td = document.createElement("td");
+      td.id = idCell(x, y);
+      row.append(td);
+      addCellListeners(td, x, y);
     }
+    table.append(row);
   }
-  show();
+  return table;
 };
+const placeBombs = () => {
+    let x,rows=[]
+
+    for(x=0;x<numBombs;x++){
+        placeSingleBombs(rows)
+    }
+    return rows
+}
+
+const placeSingleBomb = (bombs) => {
+    let row,col,nrow,ncol;
+    row=Math.floor(Math.random()*numOfRows)
+    col=Math.floor(Math.random()*numOfColumn)
+    row=bombs[row]
+
+    if(!nrow){
+        row=[]
+        bombs[row]=row
+    }
+    ncol=row[col]
+
+    if(!ncol){
+        row[col]=true
+        return
+    }else{
+        placeSingleBomb(bombs)
+    }
+}
